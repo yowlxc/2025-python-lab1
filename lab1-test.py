efeb00821fdcf88c57ed1b9ab49dc8906896040a
yowlxc <anastasiapirogova1101@gmail.com>
2025-09-14 19:18:13 +0300

    initial commit

---
+++ lab1-test.py
@@ -0,0 +1,40 @@
+import unittest
+
+def sum_of_2(nums, target):
+    if int(len(num)) < 2: return None 
+    for x in nums: if int(x) != x: return None 
+    for i in range(0, int(len(nums)) - 1):
+        for j in range (i + 1, int(len(nums))):
+            if nums[i] + nums[j] == target:
+                res = []
+                res = res + [i] + [j]
+                return res
+    return None 
+
+# Тесты
+class TestMath(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(sum_of_2([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(sum_of_2([3, 2, 4], 6), [1, 2])
+        self.assertEqual(sum_of_2([3, 3], 6), [0, 1])
+
+    def test_2_positive(self):
+        self.assertEqual(sum_of_2([7, 3, 2, 56], 48), None)             # нет подходящих чисел в массиве
+
+    def test_3_positive(self):
+        self.assertEqual(sum_of_2([5], 6), None)                        # недостаточно элементов в массиве 
+
+    def test_4_positive(self):
+        self.assertEqual(sum_of_2([3, 3.5], 6), None)                   # в массиве есть не int-овые элементы (float)
+
+    def test_5_negative(self):
+        self.assertEqual(sum_of_2([7, 3, 2, 56], 6), [1, 2])            # нет подходящих чисел в массиве
+
+    def test_6_negative(self):
+        self.assertEqual(sum_of_2([], 0), [0, 1])                        # недостаточно элементов в массиве 
+
+    def test_7_negative(self):
+        self.assertEqual(sum_of_2([3, 3], 6), [0, 1])                   # в массиве есть не int-овые элементы (string)
+
+
+
---
+++ lab1.py
@@ -0,0 +1,14 @@
+def sum_of_2(nums, target):
+    if int(len(num)) < 2: return None                           # проверка на пустой массив/массив с одним элементом
+    for x in nums: if int(x) != x: return None                  # проверка на другой тип данных (не int)
+    for i in range(0, int(len(nums)) - 1):                      # проход по массиву
+        for j in range (i + 1, int(len(nums))):                 # проход по массиву от индекса i до конца (перебор всех пар массива)
+            if nums[i] + nums[j] == target:
+                res = []
+                res = res + [i] + [j]
+                return res
+    return None                                                 # None, когда не нашлись подходящие по условию числа в массиве
+
+list = [2, 7, 11, 15]
+print (sum_of_2(list, 9))
+

c8c49ea00e9c574123d7e7ab471ee0de3d9438ae
Анастасия Пирогова <anastasiapirogova1101@gmail.com>
2025-09-26 00:02:01 +0300

    Update lab1-test.py

--- lab1-test.py
+++ lab1-test.py
@@ -1,8 +1,9 @@
 import unittest
 
 def sum_of_2(nums, target):
-    if int(len(num)) < 2: return None 
-    for x in nums: if int(x) != x: return None 
+    if int(len(nums)) < 2: return None 
+    for x in nums: 
+        if int(x) != x: return None 
     for i in range(0, int(len(nums)) - 1):
         for j in range (i + 1, int(len(nums))):
             if nums[i] + nums[j] == target:
@@ -11,7 +12,7 @@ def sum_of_2(nums, target):
                 return res
     return None 
 
-# Тесты
+# тесты
 class TestMath(unittest.TestCase):
     def test_1(self):
         self.assertEqual(sum_of_2([2, 7, 11, 15], 9), [0, 1])
@@ -19,22 +20,22 @@ class TestMath(unittest.TestCase):
         self.assertEqual(sum_of_2([3, 3], 6), [0, 1])
 
     def test_2_positive(self):
-        self.assertEqual(sum_of_2([7, 3, 2, 56], 48), None)             # нет подходящих чисел в массиве
+        self.assertEqual(sum_of_2([7, 3, 2, 10], 10), [0, 1])                     # крайние значение слева
 
     def test_3_positive(self):
-        self.assertEqual(sum_of_2([5], 6), None)                        # недостаточно элементов в массиве 
+        self.assertEqual(sum_of_2([1, 1, 2, 3, 5, 8, 13, 21], 34), [6, 7])        # крайние значения справа 
 
     def test_4_positive(self):
-        self.assertEqual(sum_of_2([3, 3.5], 6), None)                   # в массиве есть не int-овые элементы (float)
+        self.assertEqual(sum_of_2([3, 3, 3, 3, 3, 3], 6), [0, 1])                 # любые два различных индекса подходят под условие
 
     def test_5_negative(self):
-        self.assertEqual(sum_of_2([7, 3, 2, 56], 6), [1, 2])            # нет подходящих чисел в массиве
+        self.assertEqual(sum_of_2([7, 3, 2, 56], 6), None)                        # нет подходящих чисел в массиве
 
     def test_6_negative(self):
-        self.assertEqual(sum_of_2([1], 0), [0, 1])                        # недостаточно элементов в массиве
+        self.assertEqual(sum_of_2([1], 0), None)                                  # недостаточно элементов в массиве
 
     def test_7_negative(self):
-        self.assertEqual(sum_of_2(['456', 3], 6), [0, 1])                   # в массиве есть не int-овые элементы (string)
+        self.assertEqual(sum_of_2(['456', 3], 6), None)                           # в массиве есть не int-овые элементы (string)
 
-
-
+if __name__ == '__main__':
+    unittest.main()
